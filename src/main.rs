0035f28a4266bc3e1660349d94c1753a9150d5fa
thenlevy <nicolas@stockly.ai>
2026-05-07 21:33:05 +0200

    pings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
-#![allow(unused_imports)]
-use std::net::TcpListener;
+use {
+    std::{io, net::SocketAddr},
+    tokio::{
+        io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+        net::{TcpListener, TcpStream},
+    },
+};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() {
+    let socket_address = "127.0.0.1:6379";
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    let listener = TcpListener::bind(socket_address)
+        .await
+        .unwrap_or_else(|e| panic!("Failed to bind to {socket_address}: {e}"));
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, address)) => {
+                tokio::spawn(handle_connection(stream, address));
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error: {e}");
             }
         }
     }
+}
+
+async fn handle_connection(stream: TcpStream, address: SocketAddr) -> Result<(), io::Error> {
+    let (input, mut output) = stream.into_split();
+    let mut lines = BufReader::new(input).lines();
+
+    while let Some(cmd) = lines.next_line().await? {
+        match cmd.as_str() {
+            "PING" => {
+                output.write_all(b"+PONG\r\n").await?;
+            }
+            cmd => {
+                println!("unexpected command from address {address}: {cmd}");
+            }
+        }
+    }
+
+    Ok(())
 }
